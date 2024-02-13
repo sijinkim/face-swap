d66141cc428dcb47a3624ff275860fab28d930d1
sijinkim <tlwlsdi0306@gmail.com>
2024-02-13 23:43:50 +0900

    [add] Frame object 리스트로 불러오기 기능 추가 (#3)

--- src/service/extraction.py
+++ src/service/extraction.py
@@ -3,19 +3,10 @@ import cv2
 from pathlib import Path
 
 def get_frames_from_video(video: Video) -> None:
-    """
-        video를 입력받아 frame을 추출 후 저장함(jpg)
-        추출 된 frame은 video가 위치한 곳에 video 이름과 동일한 폴더 하위에 저장됨
-
-        Args:
-            video: Video
-        
-        Returns:
-    """
     video_path: Path = video.path
 
     if not video_path.exists():
-        raise ValueError("Video file is not exist.")
+        raise ValueError("Video file does not exist.")
 
     output_folder: Path = video_path.parent.joinpath(video_path.stem)
     output_folder.mkdir(parents=True, exist_ok=True)
@@ -36,3 +27,15 @@ def get_frames_from_video(video: Video) -> None:
         frame_number += 1
     
     cap.release()
+
+def load_frames(frame_folder: Path) -> list[Frame]:
+    if not frame_folder.exists():
+        raise ValueError("Frame folder does not exist.")
+    
+    frames: list[Frame] = []
+    for frame in frame_folder.iterdir():
+        frames.append(Frame(path=frame, source_video_path=Video(path=frame_folder + '.mp4')))
+    
+    return frames
+    
+
