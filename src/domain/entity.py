352f2ab5fbe14769c36c168fa3a9481ccd1de77a
sijinkim <tlwlsdi0306@gmail.com>
2024-02-13 16:20:56 +0900

    [add] entity 속성추가(#1)

--- src/domain/entity.py
+++ src/domain/entity.py
@@ -1,13 +1,16 @@
 from pydantic import BaseModel
+from pathlib import Path
 
 class Video(BaseModel):
-    ...
+    path: Path
 
 class Frame(BaseModel):
-    ...
+    path: Path
+    source_video_path: Path
 
 class ExtractedFaceImage(BaseModel):
-    ...
+    path: Path
+    source_frame_path: Path
 
 class FaceMask(BaseModel):
-    ...
+    source_face: ExtractedFaceImage
