a1f0c902d5da16c9dd3d489baec50884a4e014b7
sijinkim <tlwlsdi0306@gmail.com>
2024-02-13 17:08:10 +0900

    [fix] entity variable type 수정(#3)

---
+++ src/domain/__init__.py
@@ -0,0 +1,8 @@
+from .entity import Video, Frame, ExtractedFaceImage, FaceMask
+
+__all__ = [
+    "Video",
+    "Frame",
+    "ExtractedFaceImage",
+    "FaceMask",
+]
--- src/domain/entity.py
+++ src/domain/entity.py
@@ -6,11 +6,11 @@ class Video(BaseModel):
 
 class Frame(BaseModel):
     path: Path
-    source_video_path: Path
+    source_video_path: Video
 
 class ExtractedFaceImage(BaseModel):
     path: Path
-    source_frame_path: Path
+    source_frame_path: Frame
 
 class FaceMask(BaseModel):
     source_face: ExtractedFaceImage
