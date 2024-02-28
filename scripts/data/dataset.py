4ccde74399f2e6f2cf1f0ca1c151f9ae791c2a94
sijinkim <tlwlsdi0306@gmail.com>
2024-02-28 23:58:58 +0900

    [add] video2frame 처리 클래스 작성 (#8)

---
+++ scripts/data/dataset.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+
+import cv2
+
+class Video2Frame:
+    """
+    Converts video input data into frames and saves them.
+    
+    Args:
+        video_path: Video file path (.mp4, .mov)
+        save_dir: Parent folder path of the frames
+    """
+    def __init__(self, video_path: Path, save_dir: Path) -> None:
+        self.video_path = video_path
+        self.save_path = save_dir.joinpath(self.video_path.stem)
+
+    def __call__(self):
+        if not self.video_path.exists():
+            raise ValueError("Video file does not exist.")
+        
+        if not self.save_dir.exists():
+            self.save_path.mkdir(parents=True, exist_ok=True)
+        
+        cap = cv2.VideoCapture(str(self.video_path))
+
+        frame_number: int = 0
+        while True:
+            ret, frame = cap.read()
+
+            if not ret:
+                break
+
+            frame_path = self.save_path / f"{self.video_path.stem}_{frame_number}.jpg"
+            cv2.imwrite(str(frame_path), frame, [int(cv2.IMWRITE_JPEG_QUALITY), 90])
+
+            frame_number += 1
+        
+        cap.release()
+        
