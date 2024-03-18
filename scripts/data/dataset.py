ec46e3232e4655f4d51b4dbfe3373a37759db699
sijinkim <tlwlsdi0306@gmail.com>
2024-03-18 16:02:10 +0900

    [add] affine matrix 활용 left/right eye y축 level 동일한 aligned image 생성 (#8)

--- scripts/data/dataset.py
+++ scripts/data/dataset.py
@@ -72,42 +72,67 @@ class FaceExtractor:
     def align_stage(
         self,
         image: np.ndarray,
-        face: np.ndarray,
+        eyes_coord: np.ndarray,
         desired_face_width: int = 256,
+        left_eye_desired_coordinate: np.ndarray = np.array((0.37, 0.37)),
     ) -> np.ndarray:
         """
         Make the y-axis coordinate of the left and right eyes to the same level in the aligned image.
 
         Args:
             image: image to process
-            face: face coordinates from the detect stage
+            eyes_coord: a length 4 int garray of eyes coordinates from the detect stage
             desired_face_width: The output width of the aligned face image
+            left_eye_desired_coordinate: Fix coordinate ratio of left eye.
 
         Returns:
             np.ndarray: The processed image with aligned eye positions.
         """
         # apply same width, height
-        h = w = desired_face_width
+        desired_face_height = desired_face_width
 
-        # get coordinates of the center of the eyes
-        # right_eye_x > left_eye_x
-        right_eye = face[4:6]  # right_eye_x, right_eye_y
-        left_eye = face[6:8]  # left_eye_x, left_eye_y
-        eyes_center = (
-            int((right_eye[0] + left_eye[0]) // 2),
-            int((right_eye[1] + left_eye[1]) // 2),
+        # get desired right eye coordinate by using left eye
+        # apply same y level (e.g. left eye: [0.37, 0.37], right eye: [0.63, 0.37])
+        # final output image size를 1로 봤을 때, left eye/right eye 위치 (e.g. 3등분 -> 0.3x)
+        right_eye_desired_coordinate = np.array(
+            (1 - left_eye_desired_coordinate[0], left_eye_desired_coordinate[1])
         )
 
-        # calculate the angle between center of the left eye and center of the right eye.
+        # right_eye_x > left_eye_x
+        right_eye = eyes_coord[:2]
+        left_eye = eyes_coord[2:]
+
+        # calculate the angle and distance between center of the left eye and center of the right eye.
         # rotate the image based on this angle.
-        delta_x = abs(int(left_eye[0] - right_eye[0]))
-        delta_y = abs(int(left_eye[1] - right_eye[1]))
-        angle = (np.arctan(delta_y / delta_x) * 180) / np.pi
+        delta_x = right_eye[0] - left_eye[0]
+        delta_y = right_eye[1] - left_eye[1]
+        dist_between_eyes = np.sqrt(delta_x**2 + delta_y**2)
+        angles_between_eyes = (np.arctan(delta_y / delta_x) * 180) / np.pi
+        eyes_center = (
+            (left_eye[0] + right_eye[0]) // 2,
+            (left_eye[1] + right_eye[1]) // 2,
+        )
+
+        # get scaled distance between eyes
+        # final output image size(desired_face_width/height) 반영한 left/right eyes 거리
+        desired_dist_between_eyes = desired_face_width * (
+            right_eye_desired_coordinate[0] - left_eye_desired_coordinate[0]
+        )
+        scale = desired_dist_between_eyes / dist_between_eyes
 
         # define 2D matrix M
-        M = cv2.getRotationMatrix2D(eyes_center, (angle), 1.0)
-        # TODO: eye_center를 이미지 center로 맞추는 과정 필요. new_x, new_y
+        M = cv2.getRotationMatrix2D(
+            eyes_center, angles_between_eyes, scale
+        )  # shape: (2, 3), affine matrix
+
+        # https://docs.opencv.org/3.4/da/d54/group__imgproc__transform.html#gafbbc470ce83812914a70abfb604f4326
+        M[0, 2] += 0.5 * desired_face_width - eyes_center[0]  # x transition
+        M[1, 2] += (
+            left_eye_desired_coordinate[1] * desired_face_height - eyes_center[1]
+        )  # y transition
 
         # Applying the rotation to input image
-        rotated = cv2.warpAffine(image, M, (new_x, new_y))
-        return rotated
+        face_aligned = cv2.warpAffine(
+            image, M, (desired_face_width, desired_face_height), flags=cv2.INTER_CUBIC
+        )
+        return face_aligned
